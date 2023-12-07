782d9bee23f14fa446e4074c24d3ce5b872cd394
Jin Cheng <jinchengjoker@foxmail.com>
2023-12-07 18:41:08 +0800

    创建验证码并发送邮件

--- internal/controller/validation_code.go
+++ internal/controller/validation_code.go
@@ -1,8 +1,14 @@
 package controller
 
 import (
+	"fmt"
+	"mangosteen/internal/database"
+	"mangosteen/internal/database/model"
 	"mangosteen/internal/email"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,6 +20,7 @@ type EmailBody struct {
 func CreateValidationCode(ctx *gin.Context) {
 	var json EmailBody
 
+	// 获取 body 数据
 	if err := ctx.ShouldBindJSON(&json); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
@@ -21,7 +28,25 @@ func CreateValidationCode(ctx *gin.Context) {
 		return
 	}
 
-	if err := email.Send(json.Email); err != nil {
+	// 创建记录
+	code := genCode()
+	v_code := model.ValidationCode{Email: json.Email, Code: code}
+	result := database.DB.Create(&v_code)
+
+	if result.Error != nil {
+		ctx.JSON(500, gin.H{
+			"message": result.Error.Error(),
+		})
+		return
+	}
+
+	// 发送邮件
+	err := email.Send(
+		json.Email,
+		"山竹记账验证码",
+		fmt.Sprintf(`<body><h3>正在登录或注册山竹记账</h3><p>验证码：</p><h3>%s</h3><p>请勿与任何人共享此验证码，谢谢！</p><body>`, code),
+	)
+	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
 		})
@@ -31,4 +56,13 @@ func CreateValidationCode(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "发送成功",
 	})
+}
+
+func genCode() string {
+	// 设置随机数种子，以确保每次运行生成不同的随机数
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n := r.Intn(900000) + 100000
+	s := strconv.Itoa(n)
+
+	return s
 }
--- internal/email/email.go
+++ internal/email/email.go
@@ -11,15 +11,15 @@ func New() {
 	d = gomail.NewDialer("smtp.qq.com", 587, "jinchengjoker@foxmail.com", viper.GetString("email.password"))
 }
 
-func Send(to string) error {
+func Send(to string, subject string, body string) error {
 	if d == nil {
 		New()
 	}
 	m := gomail.NewMessage()
 	m.SetHeader("From", "Mangosteen <jinchengjoker@foxmail.com>")
 	m.SetHeader("To", to)
-	m.SetHeader("Subject", "Hello!")
-	m.SetBody("text/html", "Hello <b>金成</b>!")
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
 
 	return d.DialAndSend(m)
 }
